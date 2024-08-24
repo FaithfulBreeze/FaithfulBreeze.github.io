12a11bd5a517e2d7d7e4b04ae52a4d8ff8f5b288
Luis Victor Silva Santos <faithfulbreeze@outlook.com>
2024-08-24 02:56:04 +0000

    Add projects section

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,34 @@ function setMenu(e){
         modal.classList.remove('active-modal')
     }
 }
+
+let sliderFirstImage
+let maximumIndex
+let currentImage
+
+async function loadIframes(){
+    const data = await fetch('../data/iframe.json')
+    const iframes = JSON.parse(await (await data.blob()).text())
+    for(let frame of iframes){
+        const iframe = document.createElement('iframe')
+        iframe.src = frame.src
+        slider.appendChild(iframe)
+    }
+    sliderFirstImage = slider.children[0]
+    maximumIndex = slider.children.length - 1
+    currentImage = 0
+}
+
+function prevImg(){
+    if(currentImage < 0){
+        currentImage += 100
+        sliderFirstImage.style.marginLeft = `${currentImage}%`
+    }
+}
+
+function nextImg(){
+    if((-currentImage/100) < maximumIndex){
+        currentImage -= 100
+        sliderFirstImage.style.marginLeft = `${currentImage}%`
+    }
+}
