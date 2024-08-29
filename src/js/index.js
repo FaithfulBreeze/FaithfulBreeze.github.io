51a2e3ceb9dd7badd21d8ea189dcadfd57cd632f
Luis Victor Silva Santos <faithfulbreeze@outlook.com>
2024-08-29 04:54:48 +0000

    update portfolio

--- src/js/index.js
+++ src/js/index.js
@@ -11,15 +11,17 @@ function setMenu(e){
 let sliderFirstImage
 let maximumIndex
 let currentImage
+let iframes
 
 async function loadIframes(){
     const data = await fetch('../data/iframe.json')
-    const iframes = JSON.parse(await (await data.blob()).text())
+    iframes = JSON.parse(await (await data.blob()).text())
     for(let frame of iframes){
         const iframe = document.createElement('iframe')
         iframe.src = frame.src
         slider.appendChild(iframe)
     }
+    title[1].innerText = iframes[0].title
     sliderFirstImage = slider.children[0]
     maximumIndex = slider.children.length - 1
     currentImage = 0
@@ -29,6 +31,8 @@ function prevImg(){
     if(currentImage < 0){
         currentImage += 100
         sliderFirstImage.style.marginLeft = `${currentImage}%`
+        title[1].innerText = iframes[-currentImage/100].title
+        repo.href = iframes[-currentImage/100].repo
     }
 }
 
@@ -36,5 +40,7 @@ function nextImg(){
     if((-currentImage/100) < maximumIndex){
         currentImage -= 100
         sliderFirstImage.style.marginLeft = `${currentImage}%`
+        title[1].innerText = iframes[-currentImage/100].title
+        repo.href = iframes[-currentImage/100].repo
     }
 }
