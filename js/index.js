bcba5320d1be5d8d1c5087ae5d9aff8f96ca4300
Luis Victor Silva Santos <97199882+FaithfulBreeze@users.noreply.github.com>
2024-10-28 01:08:01 +0000

    firebase hosting

---
+++ js/index.js
@@ -0,0 +1,41 @@
+function setMenu(e){
+    if(!mobileMenu.classList.toString().includes('active')){
+        mobileMenu.classList.add('active')
+        modal.classList.add('active-modal')
+    }else{
+        mobileMenu.classList.remove('active')
+        modal.classList.remove('active-modal')
+    }
+}
+
+async function loadCards(){
+    const data = await fetch('../data/projects.json')
+    let projects = await data.json()
+    for(let project of projects){
+        const cardContainer = document.createElement('li')
+        const cardTitle = document.createElement('h2')
+        const cardImage = document.createElement('img')
+        const cardDetails = document.createElement('p')
+        const repositoryLink = document.createElement('a')
+        const projectLink = document.createElement('a')
+        const detailsDiv = document.createElement('div')
+
+        cardTitle.innerHTML = project.title
+        cardImage.src = project.img
+        cardDetails.innerText = project.details
+        repositoryLink.href = project.repo
+        repositoryLink.target = "_blank"
+        repositoryLink.innerHTML = "--- Github Repository ---"
+        projectLink.href = project.src
+        projectLink.target = "_blank"
+        projectLink.innerHTML = "--- Project Deploy ---"
+        
+        cardContainer.appendChild(cardTitle)
+        cardContainer.appendChild(cardImage)
+        detailsDiv.appendChild(cardDetails)
+        detailsDiv.appendChild(projectLink)
+        detailsDiv.appendChild(repositoryLink)
+        cardContainer.appendChild(detailsDiv)
+        cards.appendChild(cardContainer)
+    }
+}
